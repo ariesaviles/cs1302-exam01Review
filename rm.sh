f4b27d444b4c7199554c7f29ec33f198a2541883
Andrea Aviles (Xander) <ana50630@uga.edu>
2020-02-11 21:16:14 -0500

    fixed dir

--- rm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-#javac -d bin -cp bin src/cs1302/ce19/*.java
-#java -Dprism.order=sw -cp bin cs1302.ce19.ImageDriver
-
-git add README.md
-git commit -m "Update README.md"
-git push origin master
