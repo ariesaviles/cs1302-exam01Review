e1aceca94d2892b6a97a7827b74253c02a4b9d61
Andrea Aviles (Xander) <ana50630@uga.edu>
2020-02-12 14:19:56 -0500

    updated comments on Moveable

--- src/Moveable.java
+++ src/Moveable.java
@@ -1,8 +1,10 @@
 package cs1302.inter;
 
 public interface Moveable {
+    // visibility on variables and methods must be public for interface
+
     // variables in interfaces are static and final by default
-    boolean canMove = true;
+    public boolean canMove = true;
 
     // methods in interfaces must be abstract (or default methods)
     // default methods give you the ability to add methods without
