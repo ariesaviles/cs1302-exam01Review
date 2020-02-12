a70f044f07ed23f4febc1fbf9c4a37d69f6943de
Andrea Aviles (Xander) <ana50630@uga.edu>
2020-02-12 11:13:07 -0500

    updated import

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,13 +1,15 @@
+package cs1302.practice;
 
+import cs1302.practice.Animal;
+
+    // abstract classes are like normal concrete classes
+    // except they can have abstract methods
 public abstract class Mammal extends Animal {
 
     public Mammal(String name) {
         super(name);
     } // constructor
 
-
-    // abstract classes are like normal concrete classes
-    // except they can have abstract methods
     public abstract void sound();
 
     public abstract boolean isFarmMammal();
